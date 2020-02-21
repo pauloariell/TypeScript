f6a6b104ce01e84813531a387a9bc9d650c0dadd
pauloariell <pauloariell@live.com>
2020-02-20 23:24:28 -0300

    Ajustando estrutura do projeto

---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,30 @@
+class NegociacaoController {
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes: Negociacoes = new Negociacoes();
+
+    constructor(){
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+    }
+
+    adicionar(event: Event){
+        event.preventDefault();
+        const negociacao = new Negociacao(
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        this._negociacoes.adicionar(negociacao);
+        this._negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        });
+        console.log(negociacao);
+    }
+
+}
